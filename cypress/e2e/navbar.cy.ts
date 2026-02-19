a7dc4e7b18b6928f35a7302e8aa6694dc8544228
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-02-19 14:33:56 -0300

    refact(e2e): update header navigation tests for new navbar styles

--- cypress/e2e/navbar.cy.ts
+++ cypress/e2e/navbar.cy.ts
@@ -4,38 +4,65 @@ describe('Tests for the Header component', () => {
     });
 
     it('Verifies if the title is displayed correctly', () => {
-        cy.get('header h1').should('have.text', 'TextAlchemy');
+        cy.get('header h1').should('have.trimmed.text', 'Text Alchemy');
     });
 
     it('Verifies if the link to the home page is functioning', () => {
         cy.get('[data-testid=link-home]').click();
-        cy.url().should('include', '/');
+        cy.location('pathname').should('eq', '/');
     });
 
     it('Verifies if the link to the "About" page is functioning', () => {
         cy.get('[data-testid=link-about]').click();
-        cy.url().should('include', '/sobre');
+        cy.location('pathname').should('eq', '/sobre');
     });
 
-    it('Verifies if the links have the correct styling', () => {
-        cy.get('[data-testid=link-home]').should('have.class', 'text-font-color');
-        cy.get('[data-testid=link-about]').should('have.class', 'text-font-color');
-    });
-
-    it('Verifies if the links change style on hover', () => {
-        // eslint-disable-next-line cypress/unsafe-to-chain-command
+    it('Verifies if the links have the correct base styling classes', () => {
         cy.get('[data-testid=link-home]')
-            .trigger('mouseover')
-            .should('have.class', 'hover:text-light-green');
-        // eslint-disable-next-line cypress/unsafe-to-chain-command
+            .should('have.class', 'text-gray-400')
+            .and('have.class', 'hover:text-white')
+            .and('have.class', 'px-4')
+            .and('have.class', 'py-2')
+            .and('have.class', 'text-sm');
+
         cy.get('[data-testid=link-about]')
-            .trigger('mouseover')
-            .should('have.class', 'hover:text-light-green');
+            .should('have.class', 'text-gray-400')
+            .and('have.class', 'hover:text-white')
+            .and('have.class', 'px-4')
+            .and('have.class', 'py-2')
+            .and('have.class', 'text-sm');
+    });
+
+    it('Verifies active link styling when route is "/"', () => {
+        cy.location('pathname').should('eq', '/');
+
+        cy.get('[data-testid=link-home]')
+            .should('have.class', 'text-emerald-400')
+            .and('have.class', 'bg-emerald-500/10');
+
+        cy.get('[data-testid=link-about]')
+            .should('not.have.class', 'text-emerald-400')
+            .and('not.have.class', 'bg-emerald-500/10');
+    });
+
+    it('Verifies active link styling when navigating to "/sobre"', () => {
+        cy.get('[data-testid=link-about]').click();
+        cy.location('pathname').should('eq', '/sobre');
+
+        cy.get('[data-testid=link-about]')
+            .should('have.class', 'text-emerald-400')
+            .and('have.class', 'bg-emerald-500/10');
+
+        cy.get('[data-testid=link-home]')
+            .should('not.have.class', 'text-emerald-400')
+            .and('not.have.class', 'bg-emerald-500/10');
     });
 
     it('Verifies if the links navigate correctly on clicking', () => {
-        cy.get('[data-testid=link-home]').click();
-        cy.go('forward');
         cy.get('[data-testid=link-about]').click();
+        cy.location('pathname').should('eq', '/sobre');
+
+        cy.get('[data-testid=link-home]').click();
+        cy.location('pathname').should('eq', '/');
     });
 });
